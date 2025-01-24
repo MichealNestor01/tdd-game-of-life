842f56392c0a9613d20b9d432b11dcaa6651ead0
Micheal Nestor <michealnestor@outlook.com>
2025-01-24 21:21:11 +0000

    Added game board class

---
+++ src/game_board.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+@dataclass
+class Coordinate:
+    row: int
+    col: int
+
+# game board is used to isolate the game functionality from the board and screen implementation
+# a future user interface can read the baord's state to udpate the interface, or inherit from this 
+class GameBoard:
+    def __init__(self, rows: int = 0, cols: int = 0, live_cells: list[Coordinate] = []) -> None:
+        self._rows = rows
+        self._cols = cols
+        self._board = [[0 for col in range cols] for row in range rows]
+        for live_cell in live_cells:
+            if self._is_inside_board(live_cell):
+                self._set_cell_state(live_cell, True)
+
+    def _is_inside_board(self, coordinate: Coordinate) -> bool:
+        return (coordinate.row < self._rows and
+                coordinate.row >= 0 and
+                coordinate.col < self._cols and
+                coordinate.col >= 0)
+    
+    def _set_cell_state(self, coordinate: Coordinate, alive: bool) -> None:
+        if self._is_inside_board(coordinate):
+            self._board[coordinate.row][coordinate.col] = 1 if alive else 0
+
+    def cell_state_at_position(self, coordinate: Coordinate) -> bool:
+        if self._is_inside_board(coordinate):
+            return self._board[coordinate.row][coordinate.col] == 1
+
+    def print_board(self) -> None:
+        print("GAME OF LIFE BOARD")
+        for row in self._board:
+            print(row)
