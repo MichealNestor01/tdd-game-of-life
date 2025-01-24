ec45a3853672492ecd11a11c4e7963226b6f1463
Micheal Nestor <michealnestor@outlook.com>
2025-01-24 22:22:15 +0000

    change Coordinate dataclass to Pos

--- src/game_board.py
+++ src/game_board.py
@@ -1,20 +1,20 @@
 from dataclasses import dataclass
 
 @dataclass
-class Coordinate:
+class Pos:
     row: int
     col: int
 
-    def __add__(self, other):
-        return Coordinate(self.row+other.row, self.col+other.col)
+    def __add__(self, other: 'Pos'):
+        return Pos(self.row+other.row, self.col+other.col)
 
 # game board is used to isolate the game functionality from the board and screen implementation
 # a future user interface can read the baord's state to udpate the interface, or inherit from this 
 class GameBoard:
-    def __init__(self, rows: int = 0, cols: int = 0, live_cells: list[Coordinate] = []) -> None:
+    def __init__(self, rows: int = 0, cols: int = 0, live_cells: list[Pos] = []) -> None:
         self._rows = rows
         self._cols = cols
-        self._board = [[0 for col in range cols] for row in range rows]
+        self._board = [[0 for _ in range(cols)] for _ in range(rows)]
         for live_cell in live_cells:
             if self._is_inside_board(live_cell):
                 self._set_cell_state(live_cell, True)
@@ -30,27 +30,27 @@ class GameBoard:
                     return False
         return True
 
-    def _is_inside_board(self, coordinate: Coordinate) -> bool:
-        return (coordinate.row < self._rows and
-                coordinate.row >= 0 and
-                coordinate.col < self._cols and
-                coordinate.col >= 0)
+    def _is_inside_board(self, Pos: Pos) -> bool:
+        return (Pos.row < self._rows and
+                Pos.row >= 0 and
+                Pos.col < self._cols and
+                Pos.col >= 0)
     
-    def _set_cell_state(self, coordinate: Coordinate, alive: bool) -> None:
-        if self._is_inside_board(coordinate):
-            self._board[coordinate.row][coordinate.col] = 1 if alive else 0
+    def _set_cell_state(self, Pos: Pos, alive: bool) -> None:
+        if self._is_inside_board(Pos):
+            self._board[Pos.row][Pos.col] = 1 if alive else 0
 
     # returns true if alive, false if dead 
-    def get_cell_state_at_pos(self, coordinate: Coordinate) -> bool:
-        if self._is_inside_board(coordinate):
-            return self._board[coordinate.row][coordinate.col] == 1
+    def get_cell_state_at_pos(self, Pos: Pos) -> bool:
+        if self._is_inside_board(Pos):
+            return self._board[Pos.row][Pos.col] == 1
         return False
 
     def get_cell_state_at_pos(self, row: int, col: int) -> bool:
-        coordinate = Coordinate(row, col)
-        return self.get_cell_state_at_pos(coordinate)
+        Pos = Pos(row, col)
+        return self.get_cell_state_at_pos(Pos)
     
-    def print_board(self) -> None:
+    def print(self) -> None:
         print("GAME OF LIFE BOARD")
         for row in self._board:
             print(row)
