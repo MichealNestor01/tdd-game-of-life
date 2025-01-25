adebe3ae567ea72f1e56f70d78f30be5f8a488c2
Micheal Nestor <michealnestor@outlook.com>
2025-01-25 15:27:20 +0000

    added way to play the game to main function

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -35,8 +35,40 @@ def perform_game_step_iteration(board: GameBoard) -> GameBoard:
     return new_board
 
 def main():
-    test_board = GameBoard(5, 5, [Pos(2, 2)])
-    test_board.print()
-
+    print("Welcome to the game of life")
+    rows = int(input("enter screen rows: "))
+    cols = int(input("enter screen cols: "))
+    board  = GameBoard(rows, cols)
+    board.print()
+    print("Available commands:")
+    print("- Send a list of coordinates to toggle state: (1,2), (3,5), (3,3)")
+    print("- Continue to next itteration: just submit enter")
+    print("- Close program: x")
+    while True:
+        action = input("Enter a command: ")
+        if action == "x":
+            return
+        elif action == "":
+            board = perform_game_step_iteration(board)
+            board.print()
+        elif "(" in action:
+            try:
+                coordinates = action.split("(")
+                print(coordinates)
+                cells_to_toggle = []
+                for coord in coordinates:
+                    if "," in coord:
+                        print(coord)
+                        row = int(coord.split(",")[0])
+                        col = int(coord.split(",")[1].split(")")[0])
+                        cells_to_toggle.append(Pos(row, col))
+                for cell in cells_to_toggle:
+                    board.set_cell_state(cell, not board.get_cell_state_at_pos(cell))
+                board.print()
+            except:
+                print(f"coordinates were not formatted correctly, format like this: (1,2), (3,5), (3,3)")
+        else:
+            print(f"command '{action}' is not recognsied :(")
+            
 if __name__ == "__main__":
     main()
